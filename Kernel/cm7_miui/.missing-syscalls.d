da789d999a60ea7422337460e331454b9da7f225
Justin.D.Maggiolo <EarthBound.IAP@gmail.com>
2012-03-14 00:59:24 -0400

    addingMissingCm7.MIUI.files

---
+++ Kernel/cm7_miui/.missing-syscalls.d
@@ -0,0 +1,6 @@
+-:  include/generated/autoconf.h \
+ /home/earthbound/1samurai.multi/Kernel/cm7_miui/arch/arm/include/asm/types.h \
+ include/asm-generic/int-ll64.h \
+ /home/earthbound/1samurai.multi/Kernel/cm7_miui/arch/arm/include/asm/bitsperlong.h \
+ include/asm-generic/bitsperlong.h \
+ /home/earthbound/1samurai.multi/Kernel/cm7_miui/arch/arm/include/asm/unistd.h
---
+++ Kernel/cm7_miui/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 13 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#13 PREEMPT Sat Feb 18 23:16:31 EST 2012"
+#define LINUX_COMPILE_TIME "23:16:31"
+#define LINUX_COMPILE_BY "earthbound"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
